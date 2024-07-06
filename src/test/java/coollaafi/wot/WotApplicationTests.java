9d29bb54808c2a729c429f027fdbd1b9e84a14db
변유진 <byj7176@gmail.com>
2024-07-07 02:09:14 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/coollaafi/wot/WotApplicationTests.java
@@ -0,0 +1,13 @@
+package coollaafi.wot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
