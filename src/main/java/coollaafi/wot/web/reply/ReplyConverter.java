1fd6df6b5b825b81a5e32b50c60ea82945f8bec6
byj7176@gmail.com <byj7176@gmail.com>
2024-09-16 00:11:53 +0900

    feat: 대댓글 작성, 조회 api 구현 #15

---
+++ src/main/java/coollaafi/wot/web/reply/ReplyConverter.java
@@ -0,0 +1,39 @@
+package coollaafi.wot.web.reply;
+
+import coollaafi.wot.web.comment.Comment;
+import coollaafi.wot.web.comment.CommentResponseDTO;
+import coollaafi.wot.web.member.entity.Member;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ReplyConverter {
+    public Reply toEntity(Comment comment, Member member, String content) {
+        return Reply.builder()
+                .comment(comment)
+                .member(member)
+                .content(content)
+                .build();
+    }
+
+    public ReplyResponseDTO.ReplyCreateDTO toCreateDTO(Reply reply) {
+        return ReplyResponseDTO.ReplyCreateDTO.builder()
+                .replyId(reply.getId())
+                .createdAt(reply.getCreatedAt())
+                .build();
+    }
+
+    public List<ReplyResponseDTO.ReplyGetDTO> toGetDTO(List<Reply> replies) {
+        return replies.stream()
+                .map(reply -> new ReplyResponseDTO.ReplyGetDTO(
+                        reply.getId(),
+                        reply.getMember().getName(),
+                        reply.getMember().getProfileimage(),
+                        reply.getContent(),
+                        reply.getCreatedAt()))
+                .collect(Collectors.toList());
+    }
+}
