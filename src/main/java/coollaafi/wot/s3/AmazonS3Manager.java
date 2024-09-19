682523a9452c78acda5d193c9465d6c788dc0254
byj7176@gmail.com <byj7176@gmail.com>
2024-09-19 15:00:05 +0900

    fix: s3 파일 오류 수정 #18

--- src/main/java/coollaafi/wot/s3/AmazonS3Manager.java
+++ src/main/java/coollaafi/wot/s3/AmazonS3Manager.java
@@ -2,6 +2,7 @@ package coollaafi.wot.s3;
 
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import coollaafi.wot.config.AmazonConfig;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @Slf4j
@@ -23,17 +25,25 @@ public class AmazonS3Manager {
     private final AmazonConfig amazonConfig;
 
     public String uploadFile(String keyName, MultipartFile file, Long memberId) throws IOException {
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("업로드할 파일이 없습니다.");
+        }
+
         // 고유한 파일 이름 생성
-        String fileName = keyName + "/" + memberId + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+        String fileName = keyName + memberId + "/" + UUID.randomUUID();
 
         // 파일 메타데이터 설정(크기, 파일 타입 등)
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(file.getContentType());
         metadata.setContentLength(file.getSize());
 
-        // S3에 파일 업로드
-        amazonS3.putObject(amazonConfig.getBucket(),fileName ,file.getInputStream(), metadata);
+        try{
+            amazonS3.putObject(new PutObjectRequest(amazonConfig.getBucket(),fileName,file.getInputStream(), metadata));
+        }catch (IOException e){
+            log.error("error at AmazonS3Manager uploadFile : {}",(Object) e.getStackTrace());
+        }
 
+        // 업로드된 파일 URL 반환
         return amazonS3.getUrl(amazonConfig.getBucket(), fileName).toString();
     }
 
