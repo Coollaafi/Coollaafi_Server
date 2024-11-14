82fe6c6c213aff85154c2baaf391c0cd5583358c
byj7176@gmail.com <byj7176@gmail.com>
2024-11-14 13:45:17 +0900

    feat: 로그인시 토큰 전달 방식 변경

--- src/main/java/coollaafi/wot/web/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/coollaafi/wot/web/oauth2/OAuth2LoginSuccessHandler.java
@@ -1,31 +1,31 @@
 package coollaafi.wot.web.oauth2;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
         CustomOAuth2User customUser = (CustomOAuth2User) authentication.getPrincipal();
 
-        Map<String, Object> tokens = new HashMap<>();
-        tokens.put("accessToken", customUser.getAccessToken());
-        tokens.put("refreshToken", customUser.getRefreshToken());
-        tokens.put("isNewMember", customUser.isNewMember());
+        // JSON 응답 생성
+        String accessToken = customUser.getAccessToken();
+        String refreshToken = customUser.getRefreshToken();
+        boolean isNewMember = customUser.isNewMember();
+        String kakaoId = customUser.getName();
 
+        // JSON 형태로 응답
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(objectMapper.writeValueAsString(tokens));
+        response.getWriter().write(String.format(
+                "{\"accessToken\":\"%s\", \"refreshToken\":\"%s\", \"isNewMember\":\"%b\", \"userId\":%s}", accessToken,
+                refreshToken,
+                isNewMember, kakaoId));
     }
 }
