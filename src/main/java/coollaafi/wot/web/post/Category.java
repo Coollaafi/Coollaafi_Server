cc65cc426a0f81ffded7db4a0fae75e4489fee42
ujiiin <byj7176@gmail.com>
2024-11-23 01:50:29 +0900

    feat: ootd-카테고리 enum 변경 #34

--- src/main/java/coollaafi/wot/web/post/Category.java
+++ src/main/java/coollaafi/wot/web/post/Category.java
@@ -1,22 +1,27 @@
 package coollaafi.wot.web.post;
 
-public enum Category {
-    BLOUSE("블라우스"),
-    SHIRT("셔츠"),
-    JACKET("자켓"),
-    COAT("코트"),
-    PANTS("바지"),
-    SKIRT("치마"),
-    DRESS("원피스"),
-    SHOES("신발");
+import lombok.Getter;
 
+public enum Category {
+    BLOUSE("블라우스", "blouse"),
+    SHIRTS("셔츠", "shirts"),
+    JACKET("자켓", "jacket"),
+    HOODIE("후드집업", "hoodie"),
+    COAT("코트", "coat"),
+    PANTS("바지", "pants"),
+    SKIRT("치마", "skirt"),
+    DRESS("원피스", "dress"),
+    JEAN("청바지", "jean"),
+    SHOES("신발", "shoes");
+
+    @Getter
     private final String displayName;
 
-    Category(String displayName) {
-        this.displayName = displayName;
-    }
+    @Getter
+    private final String AIName;
 
-    public String getDisplayName() {
-        return displayName;
+    Category(String displayName, String AIName) {
+        this.displayName = displayName;
+        this.AIName = AIName;
     }
 }
