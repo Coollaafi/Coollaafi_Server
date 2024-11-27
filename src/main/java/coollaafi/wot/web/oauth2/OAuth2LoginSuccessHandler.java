9e7ee0ea194b33ece2baa10416bcfa36530c2fc2
ujiiin <byj7176@gmail.com>
2024-11-27 22:49:20 +0900

    fix: 의존성 주입 안됨 오류 수정

--- src/main/java/coollaafi/wot/web/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/coollaafi/wot/web/oauth2/OAuth2LoginSuccessHandler.java
@@ -3,19 +3,18 @@ package coollaafi.wot.web.oauth2;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     // RedirectStrategy 객체를 직접 생성
     private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-    @Value("${external-api.frontend.url}")
-    private String FRONTEND_URL;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -29,9 +28,12 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         Long memberId = customUser.getMemberId();
 
         // 프론트엔드 URL로 리다이렉트할 때 임시로 필요한 정보 전달
+        String FRONTEND_URL = "https://coollaafi-frontend.vercel.app";
         String frontendRedirectUrl = String.format(
                 "%s/login/success?accessToken=%s&refreshToken=%s&isMembershipRequired=%b&memberId=%d",
                 FRONTEND_URL, accessToken, refreshToken, isMembershipRequired, memberId);
+
+        log.info("Redirecting to: {}", frontendRedirectUrl);
 
         // 프론트엔드로 리다이렉트
         redirectStrategy.sendRedirect(request, response, frontendRedirectUrl);
